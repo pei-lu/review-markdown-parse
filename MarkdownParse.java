fb471aac8a854db3bea8498d0f8581985f7173a0
Alexander-Kourjanski <sashakrj@gmail.com>
2022-01-26 19:35:31 -0800

    More Tests

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -38,4 +38,10 @@ public class MarkdownParse {
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
+
+    public static String converter(String filename) throws IOException{
+        Path fileName = Path.of(filename);
+        String contents = Files.readString(fileName);
+        return contents;
+    }
 }
